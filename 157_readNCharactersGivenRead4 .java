7cc0cad56422c2165d9fc66b7bc4146ee9c0ff87
Tianshan <tianshan.cui@gmail.com>
2015-01-16 13:29:19 -0700

    LeetCode

---
+++ 157_readNCharactersGivenRead4 .java
@@ -0,0 +1,27 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        char[] buffer = new char[4];
+        boolean eof = false;
+        int readBytes = 0;
+        while (!eof && readBytes<n) {
+            int sz = read4(buffer);
+            if (sz<4) {
+                eof = true;
+            }
+            int bytes = Math.min(n-readBytes, sz);
+            for (int i=0; i<bytes; i++) {
+                buf[readBytes+i] = buffer[i];
+            }
+            readBytes += bytes;
+        }
+        return readBytes;
+    }
+}
