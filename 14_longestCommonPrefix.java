5acaeb814181da1a262ead764db5e0917a09e889
Tianshan <tianshan.cui@gmail.com>
2015-01-15 22:21:43 -0700

    LeetCode

---
+++ 14_longestCommonPrefix.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String res = "";
+        if (strs==null || strs.length==0) {
+            return res;
+        }
+        int minLen = Integer.MAX_VALUE;
+        for (String str: strs) {
+            minLen = Math.min(str.length(), minLen);
+        }
+        for (int i=0; i<minLen; i++) {
+            for (int j=1; j< strs.length; j++) {
+                if (strs[0].charAt(i) != strs[j].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0].substring(0, minLen);
+    }
+}
