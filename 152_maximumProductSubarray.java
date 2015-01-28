f23af279e7278aff07e16bcc32bb4a2c4cd3622a
Tianshan <tianshan.cui@gmail.com>
2015-01-28 16:05:52 -0500

    LeetCode

---
+++ 152_maximumProductSubarray.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int maxProduct(int[] A) {
+        if (A==null || A.length==0) {
+            return 0;
+        }
+        if (A.length==1) {
+            return A[0];
+        }
+        int maxLocal = A[0];
+        int minLocal = A[0];
+        int global = A[0];
+        for (int i=1; i<A.length; i++) {
+            int maxCopy = maxLocal;
+            maxLocal = Math.max(Math.max(A[i]*maxLocal, A[i]), A[i]*minLocal);
+            minLocal = Math.min(Math.min(A[i]*maxCopy, A[i]), A[i]*minLocal);
+            global = Math.max(global, maxLocal);
+        }
+        return global;
+    }
+}
