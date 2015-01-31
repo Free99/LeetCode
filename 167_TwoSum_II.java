bbe2ff3dd178f6de88341ba8d4776367912c48e5
Tianshan <tianshan.cui@gmail.com>
2015-01-31 11:12:14 -0500

    LeetCode

--- 167_TwoSum_II.java
+++ 167_TwoSum_II.java
@@ -1,16 +1,45 @@
-/*
- * Two Sum II - Input array is sorted
- *
- * Given an array of integers that is already sorted in ascending order, 
- * find two numbers such that they add up to a specific target number.
- *
- * The function twoSum should return indices of the two numbers such that 
- * they add up to the target, where index1 must be less than index2. 
- * Please note that your returned answers (both index1 and index2) are not zero-based.
- *
- * You may assume that each input would have exactly one solution.
- * 
- * Input: numbers={2, 7, 11, 15}, target=9
- * Output: index1=1, index2=2
- */
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        if (numbers==null || numbers.length<2) {
+            return null;
+        }
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i=0; i<numbers.length-1; i++) {
+            int start = i+1, end = numbers.length-1, gap = target-numbers[i];
+            while (start<=end) {
+                int m = start + ((end-start)>>1);
+                if (numbers[m]==gap) {
+                    res[0] = i+1;
+                    res[1] = m+1;
+                    return res;
+                } else if (numbers[m]>gap) {
+                    end = m-1;
+                } else {
+                    start = m+1;
+                }
+            }
+        }
+        return null;
+    }
+}
 
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers==null || numbers.length<2) {
+            return null;
+        }
+        int l = 0;
+        int r = numbers.length-1;
+        while (l<r) {
+            if (numbers[l]+numbers[r]==target) {
+                return new int[] {l+1, r+1};
+            } else if (numbers[l]+numbers[r]>target) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return null;
+    }
+}
