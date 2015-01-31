935b3f50e9b735b1a7b136e14656bbf82fba39ee
Tianshan <tianshan.cui@gmail.com>
2015-01-31 14:39:55 -0500

    LeetCode

--- 156_binaryTreeUpsideDown.java
+++ 156_binaryTreeUpsideDown.java
@@ -1,34 +1,50 @@
-/** 
- * Definition for binary tree 
- * public class TreeNode { 
- *     int val; 
- *     TreeNode left; 
- *     TreeNode right; 
- *     TreeNode(int x) { val = x; } 
- * } 
- */  
- public class Solution {
- 	public TreeNode UpsideDownBinaryTree(TreeNode root) {
- 		Stack<TreeNode> stack = new Stack<TreeNode>();
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode UpsideDownBinaryTree(TreeNode root) {
+        if (root==null) {
+            return null;
+        }
+        if (root.left==null && root.right==null) {
+            return root;
+        }
+        TreeNode newRoot = UpsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
 
- 		if (root == null) {
- 			return null;
- 		}
- 		while (root.left != null) {
- 			stack.push(root);
- 			root = root.left;
- 		}
- 		stack.push(root);
- 		while (!stack.empty()) {
- 			TreeNode node = stack.pop();
- 			if (!stack.empty()) {
- 				node.right = stack.peek();
- 				node.left = stack.peek().right;
- 			} else {
- 				node.left = null;
- 				node.right = null;
- 			}
- 		}
- 		return root;
- 	}
- }
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode UpsideDownBinaryTree(TreeNode root) {
+        TreeNode node = root, parent = null, right = null;
+        while (node!=null) {
+            TreeNode left = node.left;
+            node.left = right;
+            right = node.right;
+            node.right = parent;
+            parent = node;
+            node = left;
+        }
+        return parent;
+    }
+}
